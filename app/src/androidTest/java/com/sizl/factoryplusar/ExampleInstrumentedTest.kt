1f898769dce152db4ac9bb25f20543a8f33bab41
JungDonggun <donggun.dev@gmail.com>
2021-11-01 19:32:47 +0900

    Add ARCore module

--- app/src/androidTest/java/com/sizl/factoryplusar/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/sizl/factoryplusar/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.androidar
+package com.sizl.factoryplusar
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.androidar", appContext.packageName)
+        assertEquals("com.sizl.factoryplusar", appContext.packageName)
     }
 }
--- app/src/test/java/com/sizl/factoryplusar/ExampleUnitTest.kt
+++ app/src/test/java/com/sizl/factoryplusar/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.androidar
+package com.sizl.factoryplusar
 
 import org.junit.Test
 
